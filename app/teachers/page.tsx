18bce92352b77326e69350ccd6d8bf259d5d719c
seyitbugraerden <seyitbugraerden@gmail.com>
2024-08-27 23:27:40 +0300

    title changed

--- app/teachers/page.tsx
+++ app/teachers/page.tsx
@@ -32,11 +32,13 @@ function Section() {
       <section className="!bg-transparent text-white relative z-[99]">
         <div className="mx-auto w-screen max-w-[80%] px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
           <div className="mx-auto max-w-lg text-center">
-            <h2 className="text-3xl font-bold sm:text-4xl">Hoş Geldiniz !</h2>
+            <h2 className="text-3xl font-bold sm:text-4xl">
+              Bölümler ve Öğretmenler
+            </h2>
 
             <p className="mt-4 text-gray-300">
-              Dilediğiniz sınıfı inceleyebilir, güncelleyebilir ve öğretmen
-              listenizi güncel tutabilirsiniz.
+              Dilediğiniz bölümü inceleyebilir ve öğretmen listenizi güncel
+              tutabilirsiniz.
             </p>
           </div>
 
