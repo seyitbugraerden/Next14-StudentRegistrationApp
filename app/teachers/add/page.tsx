e7b3cebc2bd608f918c1c96c92bc673dc4621a3c
seyitbugraerden <seyitbugraerden@gmail.com>
2024-08-28 23:33:04 +0300

    add button

--- app/teachers/add/page.tsx
+++ app/teachers/add/page.tsx
@@ -1,25 +1,39 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 function TeacherAdd() {
+  const [value, setValue] = useState<any>(null);
+
+  const actionFunction = () => {
+    console.log(value);
+    setValue("");
+  };
+
   return (
     <div className="absolute top-0 left-0 w-screen h-screen flex flex-col justify-center items-center bg-slate-950 z-[100]">
-      (
       <>
         <h2 className="text-4xl text-white mb-12">Bölüm Ekle</h2>
-
-        <div className="flex flex-row gap-4">
-          <div className="flex flex-col gap-4 items-start text-white">
-            <span>Bölüm ismi</span>
-            <input
-              type="text"
-              className="border-[1px] border-neutral-200/50 outline-none px-3 py-2 text-white bg-transparent"
-              placeholder="Bölüm ismi"
-            />
+        <form action={actionFunction}>
+          <div className="flex flex-row gap-4">
+            <div className="flex flex-col gap-4 items-start text-white">
+              <span>Bölüm ismi</span>
+              <input
+                value={value}
+                onChange={(e: any) => {
+                  setValue(e.target.value);
+                }}
+                type="text"
+                className="border-[1px] border-neutral-200/50 outline-none px-3 py-2 text-white bg-transparent"
+                placeholder="Bölüm ismi"
+              />
+            </div>
           </div>
-        </div>
-        <div className="mt-12 rounded bg-cyan-500 px-12 py-3 text-sm font-medium text-slate-950 transition duration-300 hover:bg-cyan-500/60 hover:text-white focus:outline-none cursor-pointer">
-          Ekle
-        </div>
+          <div className="flex justify-center items-center">
+            <button className="mt-12 rounded bg-cyan-500 px-12 py-3 text-sm font-medium text-slate-950 transition duration-300 hover:bg-cyan-500/60 hover:text-white focus:outline-none cursor-pointer">
+              Ekle
+            </button>
+          </div>
+        </form>
       </>
     </div>
   );
